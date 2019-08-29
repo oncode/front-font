97aa76da0848c116f07f4d110264758d517e9bab
Sam Spycher <sam.spycher@deep-impact.ch>
2019-08-29 15:09:14 +0200

    Add letter n

--- public/letter-n.js
+++ public/letter-n.js
@@ -2,11 +2,65 @@ import { html, render } from "https://unpkg.com/lit-html?module";
 
 const htmlTemplate = () => html`
   <style>
+    @keyframes moveBefore {
+      0% {
+        transform: rotate(0);
+        opacity: 0;
+      }
+      100% {
+        transform: rotate(90deg);
+        opacity: 1;
+      }
+    }
+
+    @keyframes moveAfter {
+      0% {
+        transform: rotate(0);
+        opacity: 0;
+      }
+      100% {
+        transform: rotate(90deg);
+        opacity: 1;
+      }
+    }
+
     .letter {
+      position: relative;
+      width: 25px;
+      height: 3px;
+    }
+    .letter::before {
+      content: "";
+      position: absolute;
+      width: 4rem;
+      height: 0.3rem;
+      background-color: red;
+      top: 0;
+      left: 0.3rem;
+      animation: moveBefore 1s ease forwards;
+    }
+    .letter::after {
+      content: "";
+      position: absolute;
+      width: 4rem;
+      height: 0.3rem;
+      background-color: red;
+      top: 0;
+      animation: moveAfter 1s ease forwards;
+      left: -2rem;
+    }
+    .dash {
+      width: 4.5rem;
+      height: 0.3rem;
+      background-color: red;
+      position: absolute;
+      top: 0;
+      left: -1.1rem;
+      transform: rotate(60deg);
     }
   </style>
   <span class="letter">
-    N
+    <span class="dash"></span>
   </span>
 `;
 
